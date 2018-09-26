7e7e4394554e0fd23fcaab2b67cd464d5a57cce0
BlackBloodE <elton9731@gmail.com>
2018-09-26 17:36:48 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        int x = 0,y = 0,z = 1;
+        if(x == (y = z)){
+            System.out.println("0");
+        }
+    }
+}
